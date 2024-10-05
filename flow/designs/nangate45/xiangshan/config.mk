1ad727ac150dfae25d4acdf13e9b2e0f92728ca1
Janboe Ye <janboe.ye@gmail.com>
2024-10-06 07:48:55 +0800

    add xiangshan
    Signed-off-by: Janboe Ye <janboe.ye@gmail.com>

---
+++ flow/designs/nangate45/xiangshan/config.mk
@@ -0,0 +1,17 @@
+export DESIGN_NAME = XSTop
+export PLATFORM    = nangate45
+
+export DESIGN_NICKNAME = xiangshan
+export VERILOG_FILES = $(sort $(wildcard ./designs/src/$(DESIGN_NAME)/*.v))
+#export VERILOG_FILES = ./designs/src/$(DESIGN_NAME)/XSTop.v
+export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+export SYNTH_MEMORY_MAX_BITS = 7962624
+
+export CORE_UTILIZATION = 40
+export CORE_ASPECT_RATIO = 1
+export CORE_MARGIN = 5
+
+export PLACE_DENSITY_LB_ADDON = 0.25
+export TNS_END_PERCENT        = 100
+
